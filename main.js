fbac4373569d91a84664988a8a04b165cf10df81
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-22 14:09:40 +0200

    app js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var rileva=0;
+// metodo 1
+/* var rileva=0;
 $('a').click(function(){
     if(rileva == 1){
         $('.hamburger-menu').css('display','block');
@@ -7,5 +8,19 @@ $('a').click(function(){
         $('.hamburger-menu').css('display','none');
         rileva=1;
     }
-    
-});
+}); */
+
+// metodo 2
+/* var rileva=false;
+$('a').click(function(){
+    if(rileva == true){
+        $('.hamburger-menu').show('slow');
+        rileva=false;
+    }else{
+        $('.hamburger-menu').hide('slow');
+        rileva=true;
+    }
+}); */
+
+// metodo 3
+
