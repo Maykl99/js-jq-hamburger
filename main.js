8c828fe4db72e3c642170225285caa44de3cfee6
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-22 14:30:35 +0200

    app js

--- main.js
+++ main.js
@@ -1,32 +1,3 @@
-// metodo 1
-/* var rileva=0;
-$('a').click(function(){
-    if(rileva == 1){
-        $('.hamburger-menu').css('display','block');
-        rileva=0;
-    }else{
-        $('.hamburger-menu').css('display','none');
-        rileva=1;
-    }
-}); */
-
-// metodo 2
-/*  var rileva=false;
-$('.header-right>a').click(function(){
-    if(rileva == true){
-        $('.hamburger-menu').show('slow');
-        rileva=false;
-    }else{
-        $('.hamburger-menu').hide('slow');
-        rileva=true;
-    }
-}); */
-
-// metodo 3
-/*  $(".header-right>a").click(function(){
-    $(".hamburger-menu").toggle(400);
-});  */
-
 var esegui=$('.hamburger-menu');
 
 //apri
@@ -36,6 +7,8 @@ $('.header-right>a').click(function(){
 $('.close').click(function(){
     esegui.hide('slow');
 })
+
+
 
 
 
